c66e047039248842b95bcc4f8410d13d71a22df0
shikhar2203 <shikharsharma2203@gmail.com>
2023-10-10 22:39:13 +0530

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by shikhar on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
